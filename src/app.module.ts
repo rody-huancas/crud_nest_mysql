d80206fdc021ec2491ca4f977ebd099fb23932fa
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-24 18:07:37 -0500

    Add: configuration database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [ 
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        entities: [ __dirname + "/**/*.entity{.ts,.js}" ],
+        synchronize: true
+      }
+    ),
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
