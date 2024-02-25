d0a69308f893a0ad2ab931db5594d4898149d080
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-25 18:24:05 -0500

    Add: user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column({
+        unique: true,
+    })
+    username: string;
+    
+    @Column()
+    password: string;
+    
+    @Column({
+        type: "datetime",
+        default: () => "CURRENT_TIMESTAMP",
+    })
+    createdAt: Date;
+
+    @Column({
+        nullable: true
+    })
+    authStrategy: string;
+}
