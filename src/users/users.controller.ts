7edf271c1ddee76d1f86a2f77ec8b6eaa98b92cb
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-26 21:05:39 -0500

    Add: update user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -25,5 +26,10 @@ export class UsersController {
   @Delete(':id')
   deleteUser(@Param('id') id: number) {
     return this.usersService.deleteUser(+id);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() user: UpdateUserDto) {
+    return this.usersService.updateUser(+id, user);
   }
 }
