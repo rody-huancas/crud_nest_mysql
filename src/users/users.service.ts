2a947a2d6d852665441d30c2718883c7b2e41d31
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-26 20:30:42 -0500

    Add: List users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,4 +14,8 @@ export class UsersService {
     const newUser = this.userRepository.create(user);
     return this.userRepository.save(newUser);
   }
+
+  getUsers() {
+    return this.userRepository.find({})
+  }
 }
